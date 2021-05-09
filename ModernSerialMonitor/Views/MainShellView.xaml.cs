8ebc47f79234066215da7c4b47444c65fa3a64db
mas2kf3 <moto20000203@gmail.com>
2021-05-09 22:20:27 +0900

    シリアルモニタページのデザインはほぼ完成

--- ModernSerialMonitor/Views/MainShellView.xaml.cs
+++ ModernSerialMonitor/Views/MainShellView.xaml.cs
@@ -14,14 +14,56 @@ using System.Windows.Shapes;
 
 namespace ModernSerialMonitor.Views
 {
+    // ページ管理
+    public enum NavIcon
+    {
+        Monitor,
+        Setting
+    }
+
     /// <summary>
     /// MainShellView.xaml の相互作用ロジック
     /// </summary>
     public partial class MainShellView : Window
     {
+        private static IReadOnlyDictionary<NavIcon, Type> _pages = new Dictionary<NavIcon, Type>()
+        {
+            {NavIcon.Monitor, typeof(ModernSerialMonitor.Views.Pages.MonitorPageView) },
+            {NavIcon.Setting, typeof(ModernSerialMonitor.Views.Pages.SettingPageView) }
+        };
+
+        private static IReadOnlyDictionary<NavIcon, string> _headers = new Dictionary<NavIcon, string>()
+        {
+            {NavIcon.Monitor, "シリアルモニタ" },
+            {NavIcon.Setting, "設定" }
+        };
+
         public MainShellView()
         {
             InitializeComponent();
+        }
+
+        private void NavView_SelectionChanged(ModernWpf.Controls.NavigationView sender, ModernWpf.Controls.NavigationViewSelectionChangedEventArgs args)
+        {
+            try
+            {
+                // 選択されたアイテムを取得
+                var selectedItem = (ModernWpf.Controls.NavigationViewItem)args.SelectedItem;
+                // Tag取得
+                var iconName = selectedItem.Tag?.ToString();
+
+                if (Enum.TryParse(iconName, out NavIcon icon))
+                {
+                    // ヘッダ設定
+                    sender.Header = _headers[icon];
+                    // ナビゲーション
+                    ContentFrame.Navigate(_pages[icon]);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModernWpf.MessageBox.Show("例外 : {0}", ex.Message);
+            }
         }
     }
 }
