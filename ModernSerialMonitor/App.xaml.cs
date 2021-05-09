ceb216c51d0853b7b2988a9f7aa7bdc7989d72f1
mas2kf3 <moto20000203@gmail.com>
2021-05-09 15:53:25 +0900

    プロジェクト ファイルを追加します。

---
+++ ModernSerialMonitor/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ModernSerialMonitor
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var w = new ModernSerialMonitor.Views.MainShellView();
+            var vm = new ModernSerialMonitor.ViewModels.MainShellViewModel();
+
+            w.DataContext = vm;
+            w.Show();
+        }
+    }
+}
---
+++ ModernSerialMonitor/ViewModels/MainShellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ModernSerialMonitor.ViewModels
+{
+    class MainShellViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
