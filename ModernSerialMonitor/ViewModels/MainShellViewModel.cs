d31afa7dde621e95f41b4cc11d4d6b9d099d55fb
mas2kf3 <moto20000203@gmail.com>
2021-05-21 14:44:41 +0900

    メニュー操作追加 バージョン決定前保管

--- ModernSerialMonitor/ViewModels/MainShellViewModel.cs
+++ ModernSerialMonitor/ViewModels/MainShellViewModel.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
+using Reactive.Bindings;
 
 namespace ModernSerialMonitor.ViewModels
 {
     class MainShellViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        #region プロパティ
+
+        /// <summary>
+        /// 最上面に固定するフラグの管理 (デフォルトはfalse)
+        /// </summary>
+        public ReactiveProperty<bool> IsTopmostProperty { get; set; } = new(false);
+        #endregion
+
+
     }
 }
---
+++ ModernSerialMonitor/Views/Behaviors/WindowCloseBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace ModernSerialMonitor.Views.Behaviors
+{
+    class WindowCloseBehavior : Behavior<MenuItem>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.Click += ButtonClick;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.Click -= ButtonClick;
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // senderをMenuitemとしてキャストする
+            var menuitem = sender as MenuItem;
+            // キャスト出来なければ処理中断
+            if (menuitem is null)
+            {
+                return;
+            }
+
+            // 親のウィンドウを取得する
+            var window = Window.GetWindow(menuitem);
+
+            // ウィンドウを閉じる
+            window.Close();
+        }
+    }
+}
